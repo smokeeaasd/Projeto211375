ae513a1398d96f8dec330ac77cc31d754f5b53eb
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-11-30 12:29:17 -0300

    Formulário de clientes

--- Views/FrmCliente.Designer.cs
+++ Views/FrmCliente.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.chkVenda = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboCidades = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cliente:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 71);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(624, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(15, 311);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(120, 38);
+            this.btnIncluir.TabIndex = 6;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(141, 311);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(120, 38);
+            this.btnAlterar.TabIndex = 7;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(267, 311);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(120, 38);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(393, 311);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(120, 38);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(519, 311);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(120, 38);
+            this.btnFechar.TabIndex = 10;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(15, 378);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(681, 20);
+            this.txtPesquisa.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Digite o nome do Cliente para pesquisa:";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(700, 376);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 13;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.AllowUserToAddRows = false;
+            this.dgvClientes.AllowUserToDeleteRows = false;
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(15, 404);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.ReadOnly = true;
+            this.dgvClientes.Size = new System.Drawing.Size(760, 337);
+            this.dgvClientes.TabIndex = 14;
+            this.dgvClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellClick);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(15, 25);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // chkVenda
+            // 
+            this.chkVenda.AutoSize = true;
+            this.chkVenda.Location = new System.Drawing.Point(141, 27);
+            this.chkVenda.Name = "chkVenda";
+            this.chkVenda.Size = new System.Drawing.Size(115, 17);
+            this.chkVenda.TabIndex = 15;
+            this.chkVenda.Text = "Bloqueia a venda?";
+            this.chkVenda.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Cidade";
+            // 
+            // cboCidades
+            // 
+            this.cboCidades.FormattingEnabled = true;
+            this.cboCidades.Location = new System.Drawing.Point(12, 124);
+            this.cboCidades.Name = "cboCidades";
+            this.cboCidades.Size = new System.Drawing.Size(121, 21);
+            this.cboCidades.TabIndex = 17;
+            // 
+            // FrmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCliente";
+            this.ClientSize = new System.Drawing.Size(784, 753);
+            this.ControlBox = false;
+            this.Controls.Add(this.cboCidades);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkVenda);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.FrmCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.CheckBox chkVenda;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboCidades;
     }
 }
--- Views/FrmCliente.cs
+++ Views/FrmCliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto211375.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,113 @@ namespace Projeto211375.Views
 {
     public partial class FrmCliente : Form
     {
+        Cliente c;
         public FrmCliente()
         {
             InitializeComponent();
+        }
+
+        void LimpaControles()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof(TextBox))
+                {
+                    ((TextBox)control).Clear();
+                }
+            }
+        }
+
+        void CarregarGrid(string pesquisa)
+        {
+            c = new Cliente()
+            {
+                nome = pesquisa
+            };
+
+            dgvClientes.DataSource = c.Consultar();
+        }
+
+        private void FrmCliente_Load(object sender, EventArgs e)
+        {
+            LimpaControles();
+            CarregarGrid("");
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == string.Empty) return;
+
+            c = new Cliente()
+            {
+                nome = txtNome.Text,
+            };
+            c.Incluir();
+
+            LimpaControles();
+            CarregarGrid("");
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == string.Empty) return;
+
+            c = new Cliente()
+            {
+                id = int.Parse(txtID.Text),
+                nome = txtNome.Text,
+            };
+
+            c.Alterar();
+
+            LimpaControles();
+
+            CarregarGrid("");
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == string.Empty) return;
+
+            if (MessageBox.Show("Deseja excluir a Cliente?", "Exclusão", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                c = new Cliente()
+                {
+                    id = int.Parse(txtID.Text)
+                };
+
+                c.Excluir();
+
+                LimpaControles();
+
+                CarregarGrid("");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpaControles();
+
+            CarregarGrid("");
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            CarregarGrid(txtPesquisa.Text);
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dgvClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvClientes.RowCount > 0)
+            {
+                txtID.Text = dgvClientes.CurrentRow.Cells["id"].Value.ToString();
+                txtNome.Text = dgvClientes.CurrentRow.Cells["nome"].Value.ToString();
+            }
         }
     }
 }
