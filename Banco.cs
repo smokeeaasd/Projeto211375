5537fe7290bb133ea97de945b5bf0e7e41b531c4
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-11-30 14:54:30 -0300

    add tabela cliente e venda-cab

--- Banco.cs
+++ Banco.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Windows.Forms;
 
 using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 
 namespace Projeto211375
 {
@@ -59,8 +60,8 @@ namespace Projeto211375
                 Comando = new MySqlCommand(
                     @"CREATE TABLE IF NOT EXISTS Cidades (
                         id INT AUTO_INCREMENT,
-                        nome varchar(48),
-                        uf char(02),
+                        nome VARCHAR(48),
+                        uf CHAR(02),
                         PRIMARY KEY (id)
                     );", Conexao);
 
@@ -70,7 +71,7 @@ namespace Projeto211375
                 Comando = new MySqlCommand(
                     @"CREATE TABLE IF NOT EXISTS Marca (
                         id INT AUTO_INCREMENT,
-                        nome varchar(48),
+                        nome VARCHAR(48),
                         PRIMARY KEY (id)
                     );", Conexao);
 
@@ -80,7 +81,7 @@ namespace Projeto211375
                 Comando = new MySqlCommand(
                     @"CREATE TABLE IF NOT EXISTS Categoria (
                         id INT AUTO_INCREMENT,
-                        nome varchar(48),
+                        nome VARCHAR(48),
                         PRIMARY KEY (id)
                     );", Conexao);
 
@@ -89,14 +90,37 @@ namespace Projeto211375
                 Comando = new MySqlCommand(
                     @"CREATE TABLE IF NOT EXISTS Clientes (
                         id INT AUTO_INCREMENT,
-                        nome varchar(40),
+                        nome VARCHAR(40),
                         id_cidade INT,
                         data_nasc DATE,
-                        renda decimal(10, 2),
-                        cpf char(14),
-                        foto varchar(100),
+                        renda DECIMAL(10, 2),
+                        cpf CHAR(14),
+                        foto VARCHAR(100),
                         venda BOOLEAN,
                         FOREIGN KEY (id_cidade) REFERENCES Cidades(id),
+                        PRIMARY KEY (id)
+                    );", Conexao);
+
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand(
+                    @"CREATE TABLE IF NOT EXISTS VendaCab (
+                        id INT AUTO_INCREMENT,
+                        id_cliente INT,
+                        data DATE,
+                        total DECIMAL(10, 2),
+                        PRIMARY KEY (id)
+                    );", Conexao);
+
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand(
+                    @"CREATE TABLE IF NOT EXISTS VendaDet (
+                        id INT AUTO_INCREMENT,
+                        id_venda_cab INT,
+                        id_produto INT,
+                        qtde DECIMAL(10, 3),
+                        valor_unitado DECIMAL(10, 2),
                         PRIMARY KEY (id)
                     );", Conexao);
 
