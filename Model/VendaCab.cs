db9f0247e0361b24ea402e5df8d41591bbb089c5
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-11-30 14:53:51 -0300

    add VendaCab model

---
+++ Model/VendaCab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto211375.Model
+{
+    public class VendaCab
+    {
+        public int Id { get; set; }
+        public int idVendaCab { get; set; }
+        public int idProduto { get; set; }
+        public double qtde { get; set; }
+        public double valorUnitario { get; set; }
+    }
+}
