282447412e47e79faa676f79da99f928a3b6b658
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2022-12-01 14:22:29 -0300

    add Model de vendas

--- Model/VendaCab.cs
+++ Model/VendaCab.cs
@@ -1,17 +1,46 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Projeto211375.Model
 {
     public class VendaCab
     {
         public int Id { get; set; }
-        public int idVendaCab { get; set; }
-        public int idProduto { get; set; }
-        public double qtde { get; set; }
-        public double valorUnitario { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public DateTime data { get; set; }
+
+        public double total { get; set; }
+
+        public int Incluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+
+                Banco.Comando = new MySqlCommand("INSERT INTO vendaCab (idCliente, data, total) VALUES(@idCliente, @data, @total)", Banco.Conexao);
+
+
+                Banco.Comando.Parameters.AddWithValue("@idCliente", IdCliente);
+                Banco.Comando.Parameters.AddWithValue("@data", data);
+                Banco.Comando.Parameters.AddWithValue("@total", total);
+
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+                return (int)Banco.Comando.LastInsertedId;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+        }
     }
 }
---
+++ Model/VendaDet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Projeto211375.Model
+{
+    public class VendaDet
+    {
+        public int Id { get; set; }
+
+        public int IdVendaCab { get; set; }
+
+        public int IdProduto { get; set; }
+
+        public double Qtde { get; set; }
+
+        public double ValorUnitario { get; set; }
+
+        public void Incluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+
+                Banco.Comando = new MySqlCommand("INSERT INTO vendaDet(id_venda_cab, id_produto, qtde, valor_unitario) VALUES(@idVendaCab, @idProduto, @qtde, @valorUnitario)", Banco.Conexao);
+
+                Banco.Comando.Parameters.AddWithValue("@idVendaCab", IdVendaCab);
+                Banco.Comando.Parameters.AddWithValue("@idProduto", IdProduto);
+                Banco.Comando.Parameters.AddWithValue("@qtde", Qtde);
+                Banco.Comando.Parameters.AddWithValue("@valorUnitario", ValorUnitario);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.StackTrace, e.Message, MessageBoxButtons.OK);
+            }
+        }
+    }
+}
